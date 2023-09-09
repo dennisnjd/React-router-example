b173b7349459e9dbeb78d1eb99712d5da22f1303
dennisnjd <dennisnjd@gmail.com>
2023-09-09 19:19:50 +0530

    Second Commit

--- src/PageRouter.js
+++ src/PageRouter.js
@@ -20,4 +20,4 @@ const router = createBrowserRouter([
   ]
   );
 
-  export default router;    
+  export default router;   
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import PageNavbar from '../Components/PageNavbar';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
 
 
 function Profile() {
   return (
     <div>
         <PageNavbar/>
-      <h1>This is Proifle Page</h1>
       <Link to='/'>
       <button>Home</button>
       </Link>
+      <h1>This is Proifle Page</h1>
 
+     <Link to='/contact'>
+      <Button variant="success">Contact Page</Button>
+      </Link>
     </div>
   )
 }
